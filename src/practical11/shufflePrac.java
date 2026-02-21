ba732607a43928fc1db2fd9305a2fcd78362ba0f
MoAllie11 <alliegalant@outlook.com>
2026-02-21 13:14:06 +0200

    slowshuffle implemented

--- src/practical11/shufflePrac.java
+++ src/practical11/shufflePrac.java
@@ -1,9 +1,29 @@
 package practical11;
+import java.util.*;
 
 public class shufflePrac {
-    public static void main(String[] args){
-        int a = 2;
-        int b = 4;
-        System.out.println(a+b);
+    private static Random rand = new Random();
+    //slow shuffle
+    public static int[] slowShuffle(int N){
+        int [] shuffled = new int[N];
+        boolean[] isNotPresent = new boolean[N+1];
+        Arrays.fill(isNotPresent, true);
+        int count = 0;
+
+        while (count < N-1){
+            int r = rand.nextInt(N) + 1;
+            if (isNotPresent[r]){
+                shuffled[count] = r;
+                isNotPresent[r] = false;
+                count++;
+            }
+        }
+        for (int i=1; i <= N; i++){
+            if (isNotPresent[i]){
+                shuffled[N-1] = i;
+                break;
+            }
+        }
+        return shuffled;
     }
 }
