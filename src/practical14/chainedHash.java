b73cfb3627c00e6a3b7c1485c2148569029a45b0
MoAllie11 <alliegalant@outlook.com>
2026-03-02 15:11:10 +0200

    chainedhash update

--- src/practical14/chainedHash.java
+++ src/practical14/chainedHash.java
@@ -1,4 +1,12 @@
 package practical14;
+import java.util.LinkedList;
 
 public class chainedHash {
+    private int m; //no. of buckets
+    private LinkedList<Entry>[] table;
+    private int size; //no. of k/v pairs stored
+
+    private static class Entry{
+
+    }
 }
