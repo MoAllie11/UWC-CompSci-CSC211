c633039199ebb930af6f39f2db461f5e43f8deb9
MoAllie11 <alliegalant@outlook.com>
2026-03-02 14:34:05 +0200

    Updated openHash

--- src/practical14/openHash.java
+++ src/practical14/openHash.java
@@ -1,5 +1,17 @@
 package practical14;
+import java.util.LinkedList;
 
 public class openHash {
-    
+    private int m; //no. of buckets
+    private LinkedList<Entry>[] table;
+
+    private static class Entry{ //Inner class for k/v pairs
+        String key;
+        String value;
+
+        Entry(String key, String value){
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
