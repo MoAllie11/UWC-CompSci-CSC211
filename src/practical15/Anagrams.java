0d1cf518ba93aadf5d8ebd7c96fac96cd4969736
MoAllie11 <alliegalant@outlook.com>
2026-03-09 14:31:07 +0200

    Anagram update

--- src/practical15/Anagrams.java
+++ src/practical15/Anagrams.java
@@ -16,4 +16,28 @@ public class Anagrams {
         Arrays.sort(chars);
         return new String(chars);
     }
+
+    public static void main(String[] args) {
+        //use Set to avoid duplicates
+        Map<String, Set<String>> anagramDict = new HashMap<>();
+
+        //read file
+        try (BufferedReader br = new BufferedReader(new FileReader("ulysses.text"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                //split into words
+                String[] words = line.split("\\s+");
+                for (String w : words) {
+                    w = cleanWord(w);
+                    if (!w.isEmpty()) {
+                        //generate signature
+                        String key = signature(w);
+
+                        //group words by signature (unique only)
+                        anagramDict.computeIfAbsent(key, k -> new HashSet<>()).add(w);
+                    }
+                }
+            }
+        }
+    }
 }
