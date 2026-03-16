9cca4e369ed625ae04a4f4c0703ace5a9b382843
MoAllie11 <alliegalant@outlook.com>
2026-03-16 14:32:56 +0200

    heapsort update

--- src/practical16/tryHeapSort.java
+++ src/practical16/tryHeapSort.java
@@ -10,6 +10,17 @@ public class tryHeapSort {
     }
     //Bottom-up heapify
     private static void heapify(String[] arr, int n, int i){
-        
+        int largest = i;
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+
+        if(left<n && arr[left].compareTo(arr[largest])>0) largest = left;
+        if(right<n && arr[right].compareTo(arr[largest])>0) largest = right;
+
+        if (largest != i){
+            swap(arr, i, largest);
+            heapify(arr, n, largest);
+        }
     }
+    //Build heap bottom-up
 }
