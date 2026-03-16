4ab09ab0c4df0715b6480ed6566e661c8ef818d6
MoAllie11 <alliegalant@outlook.com>
2026-03-16 14:25:05 +0200

    heapsort update

--- src/practical16/tryHeapSort.java
+++ src/practical16/tryHeapSort.java
@@ -3,4 +3,13 @@ import java.io.*;
 import java.util.*;
 
 public class tryHeapSort {
+    private static void swap(String[] arr, int i, int j){
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    //Bottom-up heapify
+    private static void heapify(String[] arr, int n, int i){
+        
+    }
 }
